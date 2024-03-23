c49f12129d377dabb617e028fb3078d58b11c7ae
Luka <biniashvililuka345@gmail.com>
2024-03-24 01:33:11 +0400

    working rock paper scissors in console without saying actual what beat what

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-
+// computer random choice
 function getComputerChoice(){
     const choice = ["rock","paper","scissors"]
     const random_choice = Math.floor(Math.random() * choice.length);
     return choice[random_choice]
 }
-
-const ComputerChoice = getComputerChoice();
-console.log(ComputerChoice);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
 
 let playerSelection = prompt("Rock, Paper or Scissors?");
-playerSelection = playerSelection.toUpperCase()
+playerSelection = playerSelection.toLowerCase()
 console.log(playerSelection);
+
+function playRound(playerSelection,computerSelection) {
+    if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){ // throws out user error if they type something else
+        return "User Error! Try again!";
+    }
+    else if (playerSelection === computerSelection){ //if theres a tie
+        return "Tie!";
+    }
+    else if((playerSelection === "rock" && computerSelection === "scissors")||
+            (playerSelection === "paper" && computerSelection === "rock")||
+            (playerSelection === "scissors" && computerSelection === "paper")){
+                return "You win!"
+    }
+    else{
+        return "You Lose!"
+    }
+  }
+
+  console.log(playRound(playerSelection, computerSelection));
